a99c6cd7dfbb14de2753dd8a5658edf416a587f4
MiguelvFilipe <60987760+MiguelvFilipe@users.noreply.github.com>
2025-02-27 00:06:38 +0000

    execute request on customer list

--- src/app/component/customer-list/customer.component.ts
+++ src/app/component/customer-list/customer.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { loadAppData } from '../../store/actions/customers.actions';
+import { getLoadingState } from '../../store/selectors/customers.selectors';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
-export class CustomerComponent {
+export class CustomerComponent implements OnInit {
+  isLoading!: boolean;
+
+  
+  constructor(
+    private store: Store,
+    private router: Router,
+
+  ) {}
+
+  ngOnInit(): void {
+
+      this.store.dispatch(loadAppData());
+
+      this.store.select(getLoadingState).subscribe(isLoading => {
+        this.isLoading = isLoading;
+        console.log('Loading state:', isLoading);
+      });
+  }
+
+  
 
 }
