a39dce16f7752b99a7c71fc9010a2889eab2f00e
MiguelvFilipe <60987760+MiguelvFilipe@users.noreply.github.com>
2025-02-27 00:06:13 +0000

    main app module, store setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule,isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './component/customer-list/customer.component';
 import { CustomerDetailComponent } from './component/customer-detail/customer-detail.component';
 
+
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducer } from './store/reducers/customers.reducer';
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from "@ngrx/effects";
+import { AppEffects } from './store/effects/customers.effects';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +23,13 @@ import { CustomerDetailComponent } from './component/customer-detail/customer-de
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({ app: appReducer }),
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    StoreDevtoolsModule.instrument({ maxAge: false, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([AppEffects]),
   ],
   bootstrap: [AppComponent]
 })
