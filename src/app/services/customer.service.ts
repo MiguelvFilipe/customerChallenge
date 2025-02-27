9ff36b9a1b91879f4d2eb934e26e739fea138293
MiguelvFilipe <60987760+MiguelvFilipe@users.noreply.github.com>
2025-02-27 15:07:23 +0000

    add page and limit to API url + cache

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,5 +1,4 @@
-
-  import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
   import { Injectable } from '@angular/core';
   import { CustomerModel } from '../models/customer.model';
   import { Observable, of, BehaviorSubject } from 'rxjs';
@@ -12,22 +11,24 @@
     private customerListCache: CustomerModel[] | null = null;
     private loadingSubject = new BehaviorSubject<boolean>(false);
     private errorSubject = new BehaviorSubject<string | null>(null);
+    private cache: { [key: string]: CustomerModel[] } = {};
   
     loading$ = this.loadingSubject.asObservable();
     error$ = this.errorSubject.asObservable();
   
     constructor(private http: HttpClient) { }
   
-    GetAllCustomers(): Observable<CustomerModel[]> {
-      if (this.customerListCache) {
-        return of(this.customerListCache);
+    GetAllCustomers(page: number, limit: number): Observable<CustomerModel[]> {
+      const cacheKey = `${page}-${limit}`;
+      if (this.cache[cacheKey]) {
+        return of(this.cache[cacheKey]);
       }
   
       this.loadingSubject.next(true);
       this.errorSubject.next(null);
   
-      return this.http.get<CustomerModel[]>("https://620e9760ec8b2ee28326ae84.mockapi.io/api/1/users").pipe(
-        tap(data => this.customerListCache = data),
+      return this.http.get<CustomerModel[]>(`https://620e9760ec8b2ee28326ae84.mockapi.io/api/1/users?page=${page}&limit=${limit}`).pipe(
+        tap(data => this.cache[cacheKey] = data),
         catchError(error => {
           this.errorSubject.next('Failed to load customer data');
           return of([]);
