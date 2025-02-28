91f472153c5ea22eede297dea837362a6694ba90
MiguelvFilipe <60987760+MiguelvFilipe@users.noreply.github.com>
2025-02-28 04:53:36 +0000

    add NG-ZORRO as a ui component library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,13 @@ import { appReducer } from './store/reducers/customers.reducer';
 import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from "@ngrx/effects";
 import { AppEffects } from './store/effects/customers.effects';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+
+registerLocaleData(en);
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,6 +38,9 @@ import { AppEffects } from './store/effects/customers.effects';
     StoreDevtoolsModule.instrument({ maxAge: false, logOnly: !isDevMode() }),
     EffectsModule.forRoot([AppEffects]),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ]
 })
 export class AppModule { }
